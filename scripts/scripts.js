e33d8830843de9c0dff872bf417b8251712c5122
LorenzoElenkov <thevalorbg@gmail.com>
2022-01-07 18:52:13 +0200

    commit on 'master'

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -39,6 +39,14 @@ $(function () {
         }
     });
 
+    dropdownMenu.each(function () {
+        $(this).on("click", function () {
+            dropdownMenu.slideUp();
+            menuIcon.toggleClass("isClicked");
+            menuClicked = false;
+        });
+    });
+
     const my_projects = $("#my-projects");
     const right_arrow_projects = $("#right-indicator img");
     const right_arrow_projects_container = $("#right-indicator");
@@ -66,7 +74,6 @@ $(function () {
     );
 
     const project_tabs = $("#project-tabs");
-    console.log($(project_tabs)[0].scrollWidth + " / " + $(project_tabs).outerWidth());
     const overflowing = project_tabs[0].scrollWidth - 1 > $(project_tabs).width();
     if (overflowing) {
         right_arrow_projects_container.css({
@@ -138,8 +145,14 @@ $(function () {
         });
     });
 
+    const new_project_name = $("#new-project-name");
+
     $("#btn-add-project").on("click", function () {
-        $("<div>New Project</div>").insertBefore("#my-projects #project-tabs #left-indicator")
+        if (new_project_name.val() == "") {
+            new_project_name.val("New Project");
+        };
+
+        $(`<div>${new_project_name.val()}</div>`).insertBefore("#my-projects #project-tabs #left-indicator")
         if($(project_tabs)[0].scrollWidth > $(project_tabs)[0].clientWidth) {
             right_arrow_projects_container.css(
                 {
@@ -147,18 +160,122 @@ $(function () {
                 }
             )
         }
-    });
-
-    const create_project_menu = $("#create-project-menu");
-
-    $("#btn-create-project").on("click", function () {
         create_project_menu.css(
             {
-                "display": "block"
+                "display": "none"
             }
         )
     });
 
+    const create_project_menu = $("#create-project-menu");
+    const create_project_close_desktop = $("#btn-close-create-proj");
+    const new_project_lang = $("#new-project-lang");
+    const new_project_lang_predict = $("#new-project-lang-predict-tab");
+    const new_project_lang_container = $(".new-project-lang-container");
+
+    $("#btn-create-project").on("click", function () {
+        new_project_name.val("");
+        create_project_menu.css(
+            {
+                "display": "block"
+            }
+        );
+        new_project_name.focus();
+    });
+
+    create_project_close_desktop.on("click", function () {
+        create_project_menu.css(
+            {
+                "display": "none"
+            }
+        )
+        new_project_lang.val("");
+        new_project_lang_predict.empty();
+    })
+
+    let langArray = ["html", "css", "javascript", "jquery", "sass/scss", "less", "react.js", "angular", "vue.js", "mongodb", "localstorage", "postgresql", "other"];
+    let selectChild = 0;
+    let onlyOneChild = false;
+    let selected;
+
+    
+    
+    new_project_lang.on("keyup", function (e) {
+        if (e.key !== "ArrowDown") {
+            if (new_project_lang.val().length <= 20 && new_project_lang.val().length > 0) {
+                if (selected == undefined || (selected != undefined && selected != new_project_lang_predict.children(":last-child").html()) || (selected != undefined && new_project_lang_predict.children(":first-child").hasClass("predict-tab-hover") && !onlyOneChild)) {
+                
+                
+                    new_project_lang_predict.empty();  
+                    for (i = 0; i < langArray.length; i++) {
+                        if (langArray[i].includes(new_project_lang.val())) {
+                            new_project_lang_predict.append(`<div>${langArray[i]}</div>`);
+                        }
+                    };
+                }
+            } 
+        } else if (e.key !== "ArrowUp") { 
+            if (new_project_lang.val().length <= 20 && new_project_lang.val().length > 0) {
+                if (selected == undefined || (selected != undefined && selected != new_project_lang_predict.children(":last-child").html()) || (selected != undefined && new_project_lang_predict.children(":first-child").hasClass("predict-tab-hover") && !onlyOneChild)) {
+                
+                
+                    new_project_lang_predict.empty();  
+                    for (i = 0; i < langArray.length; i++) {
+                        if (langArray[i].includes(new_project_lang.val())) {
+                            new_project_lang_predict.append(`<div>${langArray[i]}</div>`);
+                        }
+                    };
+                }
+            } 
+        } else if (new_project_lang.val().length == 0) {
+            new_project_lang_predict.empty();
+            selectChild = 0;
+        } 
+        
+        if (e.key === "ArrowDown") {
+            if (new_project_lang_predict.children().length > 0 && selectChild < new_project_lang_predict.children().length && new_project_lang.val().length <= 20) {
+                ++selectChild;
+                
+                if (new_project_lang_predict.children(":first-child").hasClass(".predict-tab-hover")) {
+                    onlyOneChild = true;
+                    console.log("ONLY ONE CHILD TRUE");
+                }
+
+                if (selected != new_project_lang_predict.children(":last-child").html()) {
+                    new_project_lang_predict.children(`:nth-child(${selectChild})`).addClass("predict-tab-hover");
+                    new_project_lang_predict.children(`:nth-child(${selectChild-1})`).removeClass("predict-tab-hover");    
+                }
+                selected = new_project_lang_predict.children(`:nth-child(${selectChild})`).html();
+    
+            }
+        } else if (e.key === "ArrowUp") {
+            if (new_project_lang_predict.children().length > 0 && selectChild > 1 && new_project_lang.val().length <= 20) {
+                --selectChild;
+                if (selected != new_project_lang_predict.children(":first-child")) {
+                    new_project_lang_predict.children(`:nth-child(${selectChild})`).addClass("predict-tab-hover");
+                    new_project_lang_predict.children(`:nth-child(${selectChild+1})`).removeClass("predict-tab-hover");    
+                }
+                selected = new_project_lang_predict.children(`:nth-child(${selectChild})`).html();
+            
+            }
+        }
+        if (e.key === "Enter") {
+            if (new_project_lang_predict.children().length > 0 && selected != undefined) {
+                new_project_lang_predict.empty();
+                selectChild = 0;
+                new_project_lang.val("");
+                if (new_project_lang_container.children().length == 0) {
+                    new_project_lang_container.html("");
+                }
+                new_project_lang_container.append(`<div>${selected}</div>`);
+                selected = undefined;
+                firstMarked = false;
+            }
+        }
+    });
+
+
+    
 
 
 
